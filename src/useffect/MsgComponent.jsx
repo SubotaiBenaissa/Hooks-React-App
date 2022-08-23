0c6fe5d61bc140cda4d500d5e8b466f1862a8e81
Alangobe <alangobe01@gmail.com>
2022-08-23 14:34:58 -0500

    useEffect raro terminado xd

--- src/useffect/MsgComponent.jsx
+++ src/useffect/MsgComponent.jsx
@@ -4,15 +4,21 @@ export const MsgComponent = () => {
 
     useEffect(() => {
 
-        console.log('MsgComponent montado');            // Cuando se monta el componente se muestra el mensaje  
+        const onMouseMove = ( { x, y } ) => {                    // Para poder hacer limpieza con el cleanup se tiene que crear una referencia al listener que se quiere parar
+            const coords = { x, y };
+            console.log(coords);
+        }                     
+        console.log('MsgComponent montado');                     // Cuando se monta el componente se muestra el mensaje y se ejecuta el listener
+        window.addEventListener('mousemove', onMouseMove)            
         
         return () => {
 
-            console.log('MsgComponent desmontado');     // Este es el cleanup y se ejecuta cuando el useEffect termina, en este caso, cuando se desmonta el componente :D
+            console.log('MsgComponent desmontado');                 // Este es el cleanup y se ejecuta cuando el useEffect termina, en este caso, cuando se desmonta el componente :D
+            window.removeEventListener('mousemove', onMouseMove);   // Se para el listener de mousemove
 
         }
 
-    }, [])                                              // Array vacío para indicar que se ejecutará el useEffect cuando se monte el componente
+    }, []);                                             // Array vacío para indicar que se ejecutará el useEffect cuando se monte el componente
     
 
     return (
